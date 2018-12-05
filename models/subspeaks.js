1e04f5f94e0f83c919e87021902258a31c9772bd
Bobby Boyd <bboyd2008@gmail.com>
2018-12-04 23:05:27 -0800

    Setting things up

---
+++ models/subspeaks.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+  var Example = sequelize.define("Subspeaks", {
+    sp_id: DataTypes.INTEGER
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
+  });
+  return Example;
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,14 +11,30 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+  //the login route
+  app.get("/login", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("authentication", {});
+    });
+  });
+
+  //register route
+  app.post("/register", function(req, res) {});
+  //route for the subspeaks
+  //This will display the posts of that specific subspeak
+  app.get("/s/:username", function(req, res) {
+    db.Example.findOne({ where: { username: req.params.username } }).then(function(
+      dbExample
+    ) {
+      res.render("s", {
         example: dbExample
       });
     });
   });
+
+  app.get("/s/:subspeak/:post", function(req, res){
+    db.Example.findOne({ where: { subspeak: req.params.subspeak }} )  
+  })
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
