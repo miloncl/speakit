c23b08ee162b5cdd130b226561cdb8f313e0d012
Julian Rambob <jlrambob@ucdavis.edu>
2018-12-08 13:15:30 -0800

    updated api routes for tables

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -118,23 +118,23 @@ module.exports = function(app) {
     });
   })
 
-  app.post("/s/subspeaks", function(req, res){
-    db.Subspeaks.create({ description: 'foo', views: 'bar', numberofsub: 5, icon: 'hello' }).then(task => {
+  // app.post("/s/subspeaks", function(req, res){
+  //   db.Subspeaks.create({ description: 'foo', views: 'bar', numberofsub: 5, icon: 'hello' }).then(task => {
+  //     // you can now access the newly created task via the variable task
+  //   })
+  // })
+
+  // app.post("/s/posts", function(req, res){
+  //   db.Post.create({ post_text: 'foo', tags: 'bar', categories: '', views: 5, title: '', }).then(task => {
+  //     // you can now access the newly created task via the variable task
+  //   })
+  // })
+
+  app.post("/s/comments", function(req, res){
+    db.Comments.create({ comments: 'foo', voting: 'bar',}).then(task => {
       // you can now access the newly created task via the variable task
     })
   })
-
-  // app.post("/s/posts", function(req, res){
-  //   db.Post.create({ post_text: 'foo', tags: 'bar', categories: '', views: '', title: '', }).then(task => {
-  //     // you can now access the newly created task via the variable task
-  //   })
-  // })
-
-  // app.post("/s/comments", function(req, res){
-  //   db.Comments.create({ comments: 'foo', voting: 'bar',}).then(task => {
-  //     // you can now access the newly created task via the variable task
-  //   })
-  // })
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
